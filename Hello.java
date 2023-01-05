f54bcbce122dce405a29fd1e284e8888575b0918
Sammar Abbas <42285767+8sagh8@users.noreply.github.com>
2023-01-05 16:10:21 -0500

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 // Your First Program
 
-class HelloWorld {
+public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello, World! from Sammar Abbas in Java"); 
     }
 }
